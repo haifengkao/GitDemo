bb20eb69c70b6b008ab2d61d95a35369ec5b4c09
Lono <Lono@LonodeMacBook-Pro.local>
2015-10-06 17:56:55 +0800

    first commit

---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Lono on 2015/10/6.
+//  Copyright © 2015年 cocoaspice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitDemo
+//
+//  Created by Lono on 2015/10/6.
+//  Copyright © 2015年 cocoaspice. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
